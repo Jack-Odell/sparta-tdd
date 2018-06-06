d63e57904a39337728ec8c67eb55ed444d98722f
Tech-A48 <tech-a48@Tech-A48s-MacBook-Air.local>
2018-06-06 15:13:25 +0100

    Test complete on checking sum below 10

--- project_euler/multiples_3_5/lib/mult_three_five.rb
+++ project_euler/multiples_3_5/lib/mult_three_five.rb
@@ -1,2 +1,26 @@
 class MultThreeFive
+  attr_accessor :multiples_arr
+
+  def initialize
+    @multiples_arr = []
+  end
+
+
+  def create_multiples_arr(num_from, num_to)
+    (num_from..num_to).each do |i|
+      if divisible_by?(i, 3) && i < num_to
+        @multiples_arr << i
+      elsif divisible_by?(i, 5) && i < num_to
+        @multiples_arr << i
+      end
+    end
+  end
+
+  def divisible_by?(num, div_by)
+    (num % div_by).zero?
+  end
+
+  def sum_of_multiples
+    @multiples_arr.sum
+  end
 end
--- project_euler/multiples_3_5/spec/mult_three_five_spec.rb
+++ project_euler/multiples_3_5/spec/mult_three_five_spec.rb
@@ -1,5 +1,30 @@
-require 'spec_helper'
+require 'mult_three_five'
 
-describe do
-  
+describe MultThreeFive do
+
+  before(:all) do
+    @mtf = MultThreeFive.new
+    @mtf.create_multiples_arr(1, 10)
+  end
+
+  it 'Should return true if number is divisible by 3' do
+    expect(@mtf.divisible_by?(9, 3)).to be true
+  end
+
+  it 'Should return false if number is not divisible by 3' do
+    expect(@mtf.divisible_by?(10, 3)).to be false
+  end
+
+  it 'Should return true if number is divisible by 5' do
+    expect(@mtf.divisible_by?(10, 5)).to be true
+  end
+
+  it 'Should return false if number is not divisible by 5' do
+    expect(@mtf.divisible_by?(11, 5)).to be false
+  end
+
+  it 'Should return sum of all multiples of three and five from one to ten equaling twenty-three' do
+    expect(@mtf.sum_of_multiples).to eq(23)
+  end
+
 end
